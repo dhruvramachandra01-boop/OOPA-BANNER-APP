e868bed86622d8469b46d55799d404821c0dc034
Dhruv <dhruvramachandra01@gmail.com>
2026-04-10 00:39:14 +0530

    adding uc3 file

---
+++ OOPS-Banner-App.java
@@ -0,0 +1,25 @@
+
+/**
+ * OOPS Banner App
+ * UC3 - Print OOPS Banner using String.join() 
+ * @author Dhruv R
+ * @version 3.0
+ */
+
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+
+
+        System.out.println("OOPS");
+
+        System.out.println(String.join("   ***  ","    ***   ","  ******  ","   *****  ");
+        System.out.println(String.join("  **  ** ","  **  ** ","  **   **  "," **      ");
+        System.out.println(String.join(" **    ** ","**    ** "," **   **  "," **      ");
+        System.out.println(String.join(" **    ** ","**    **  ","******     "," ***   ");
+        System.out.println(String.join(" **    ** ","**    **  ","**           ","  ** ");
+        System.out.println(String.join("  **  **   ","**  **   ","**           ","  ** ");
+        System.out.println(String.join("   ***      ","***     ","**        "," *****  ");
+
+
+    }
+}
